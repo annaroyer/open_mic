a46474aaf4085ccc797ab389c6c5931783f857c6
Anna Royer <anna.royer@colorado.edu>
2017-12-13 09:16:18 -0700

    iteration 1

--- test/user_test.rb
+++ test/user_test.rb
@@ -1,6 +1,7 @@
 require 'minitest'
 require 'minitest/autorun'
 require 'minitest/pride'
+require './lib/joke'
 require './lib/user'
 
 class UserTest < Minitest::Test
@@ -20,5 +21,26 @@ class UserTest < Minitest::Test
     ali = User.new("Ali")
 
     assert_equal "Ali", ali.name
+  end
+
+  def test_it_starts_with_no_jokes
+    sal = User.new('Sal')
+
+    result = sal.jokes
+
+    assert_equal [], result
+  end
+
+  def test_learn_joke_gives_it_a_new_joke
+    sal = User.new('Sal')
+    joke = Joke.new({ id: 1,
+                      question: "Why did the strawberry cross the road?",
+                      answer: "Because his mother was in a jam."})
+    sal.learn(joke)
+
+    result = sal.jokes
+
+    assert_equal 1, result.length
+    assert_equal [joke], result
   end
 end
